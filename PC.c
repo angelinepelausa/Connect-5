7abecd6c8a2be196186bc3fd5475126b30033e5d
ligaya-milangela <125577516+ligaya-milangela@users.noreply.github.com>
2024-05-03 18:19:26 +0800

    PC1.c

--- PC.c
+++ PC.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
+#include <ctype.h>
+
+void block(int col, char table[9][9]){
+    for(int i=8; i>=0; i--){
+        if (table[i][col] == ' '){
+            table[i][col] = '1';
+            break;
+        }
+    }
+}
 
 int main() {
     int rows = 9;
     int cols = 9;
+    char c;
     
     char table[rows][cols];
-    
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
             table[i][j] = ' ';
         }
     }
+    while(1){
     
     for(int j = 1; j <= cols; j++) {
         table[0][j] = 'A' + j-1;
@@ -26,6 +37,35 @@ int main() {
         }
         printf("\n");
     }
-    
+    printf("Enter column: ");
+    scanf("%c", &c);
+    switch (tolower(c)){
+        case 'a':
+            block(1, table);
+            break;
+        case 'b':
+            block(2, table);
+            break;
+        case 'c':
+            block(3, table);
+            break;
+        case 'd':
+            block(4, table);
+            break;
+        case 'e':
+            block(5, table);
+            break;
+        case 'f':
+            block(6, table);
+            break;
+        case 'g':
+            block(7, table);
+            break;
+        case 'h':
+            block(8, table);
+            break;
+        default:
+            printf("Invalid Input\n");
+    }}
     return 0;
 }
